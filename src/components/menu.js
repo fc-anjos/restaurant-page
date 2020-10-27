8af8ae7f623bf9b48ff806d76435c91ccea3e27a
fc-anjos <felipe.anjos@usp.br>
2020-10-27 18:14:31 -0300

    Organize content in menu page

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,4 +1,4 @@
-
+import styles from '../styles/menu.module.css';
 import { LoremIpsum } from '../../node_modules/lorem-ipsum';
 
 const randomFromInterval = (min, max) => Math.floor(Math.random() * (max - min + 1 + min));
@@ -23,40 +23,63 @@ const genMenuEntry = () => {
   return menuEntry;
 };
 
-const genMenuEntries = () => {
+const genMenuEntries = nEntries => {
   const menuEntries = [];
-  for (let i = 0; i < 20; i++) {
+  for (let i = 0; i < nEntries; i += 1) {
     menuEntries.push(genMenuEntry());
   }
   return menuEntries;
 };
 
-const menu = genMenuEntries();
 
 const MenuItemTag = menuItem => {
   const { item, description, price } = menuItem;
   return `
-  <div>
-  <h3>
-    ${item}
-  </h3>
+  <div class="${styles.menuEntry}">
+    <div class="${styles.topLine}">
+      <h4>
+        ${item}
+      </h4>
+      <span class="${styles.price}">
+        ${price}$
+      </span>
+    </div>
   <span>
     ${description}
-  </span>
-  <span>
-    ${price}
   </span>
 </div>
 `;
 };
 
-const MenuItemTags = menu.map(MenuItemTag).join('');
 
-const AboutContent = menuItem => `
-<div>
+const entrees = genMenuEntries(5);
+const EntreesTags = entrees.map(MenuItemTag).join('');
+
+const mains = genMenuEntries(7);
+const mainTags = mains.map(MenuItemTag).join('');
+
+const cocktails = genMenuEntries(4);
+const cocktailsTags = cocktails.map(MenuItemTag).join('');
+
+const MenuContent = () => `
+<div class="${styles.menuContainer}">
   <h2>Menu</h2>
-  ${MenuItemTags}
+
+  <div>
+    <h3>Entrées</h3>
+    ${EntreesTags}
+  </div>
+
+  <div>
+    <h3>Main Dishes</h3>
+    ${mainTags}
+  </div>
+
+  <div>
+    <h3>Cocktails</h3>
+    ${cocktailsTags}
+  </div>
 </div>
 `;
 
-export default AboutContent;
+export default MenuContent;
